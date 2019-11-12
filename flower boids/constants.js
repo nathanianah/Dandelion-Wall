58c389e7f31eb11e5e8d97a595b5c87839d9317a
Nathania <nathanianah@gmail.com>
2019-11-11 21:52:48 -0500

    random flowers from json appear/disappear with boids

--- flower boids/constants.js
+++ flower boids/constants.js
@@ -2,8 +2,8 @@ const radius = 10.0;
 const maxForce = 0.03;    // Maximum steering force
 const maxVelocity = 1;    // Maximum speed
 
-const caWeight = 3;
+const caWeight = 10;
 const vmWeight = 1.0;
 const fcWeight = 1.0;
 
-const collisionDistance = 3 * radius;
+const collisionDistance = 5 * radius;
--- flower boids/main.js
+++ flower boids/main.js
@@ -1,6 +1,7 @@
-var flock;
 var numFlowers;
 var json;
+var boids = [];
+var flowers = [];
 
 function preload() {
   json = loadJSON("flower.json");
@@ -8,45 +9,44 @@ function preload() {
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	
-  flock = new Flock();
   // Initialize with 20 boids in the center
   for (let i = 0; i < 20; i++) {
-    flock.addBoid(new Boid(windowWidth/2,windowHeight/2));
+    addBoid(new Boid(random(width),random(height)));
   }
 }
 
 function draw() {
   background(255);
-  // flock.run();
+	
+	for (let i = 0; i < flowers.length; i++) {
+    flowers[i].update(boids[i].location); //draw the flowers
+  }
+
+  for (let i = flowers.length -1; i > -1; i--) {
+    if (flowers[i].flowerScale < 0.02) {
+      flowers.splice(i, 1); //if the flower is very small, kill it
+			boids.splice(i,1);
+    }
+  }
+  run();
 }
 
 // Add a new boid into the System
 function mousePressed() {
-  flock.addBoid(new Boid(mouseX,mouseY));
+  addBoid(new Boid(mouseX,mouseY));
 }
 
-class Flock {
+function run() {
+	for (let i = 0; i < this.boids.length; i++) {
+		this.boids[i].run(this.boids);  // Passing the entire list of boids to each boid individually
+		// this.flowers[i].setLoc(this.boids[i].location);
+		// this.flowers[i].update();
+	}
+}
 
-  constructor () {
-    this.boids = []; // Initialize the ArrayList
-		this.flowers = [];
-  }
-
-  run() {
-    for (let i = 0; i < this.boids.length; i++) {
-      this.boids[i].run(this.boids);  // Passing the entire list of boids to each boid individually
-			this.flowers[i].xpos = this.boids[i].location.x;
-			this.flowers[i].ypos = this.boids[i].location.y;
-			this.flowers[i].update();
-    }
-  }
-
-  addBoid(b) {
-    this.boids.push(b);
-		this.flowers.push(new Flower(random(5),b.location.x, b.location.y));
-  }
-
+function addBoid(b) {
+	this.boids.push(b);
+	flowers.push(new GeneratedFlower(Math.floor(random(5)), width / 2, height / 2));
 }
 
 
